d19f79bab224b913276d5861e2b73651ee96a6b8
Ulrich Raab <ulraab@gmail.com>
2017-03-17 12:43:22 +0100

    Improved example app

--- app/src/main/java/de/ulrichraab/arrow/app/presentation/MainActivity.java
+++ app/src/main/java/de/ulrichraab/arrow/app/presentation/MainActivity.java
@@ -3,15 +3,51 @@ package de.ulrichraab.arrow.app.presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
+import javax.inject.Inject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.ulrichraab.arrow.Arrow;
 import de.ulrichraab.arrow.app.R;
+import de.ulrichraab.arrow.app.model.Device;
+import de.ulrichraab.arrow.app.model.User;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    @Inject Device device;
+    @Inject Long random;
+    @Inject User user;
+
+    @BindView(R.id.device) TextView deviceTextView;
+    @BindView(R.id.random) TextView randomTextView;
+    @BindView(R.id.user) TextView userTextView;
+
     @Override
     protected void onCreate (Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Bind views
+        ButterKnife.bind(this);
+
+        // Inject dependencies
+        Arrow.createInjectorBuilder(this, MainActivityInjector.Builder.class)
+             .user(new User("John Doe"))
+             .build()
+             .injectMembers(this);
+    }
+
+    @Override
+    protected void onResume () {
+        super.onResume();
+
+        deviceTextView.setText(device.name());
+        randomTextView.setText(String.valueOf(random));
+        userTextView.setText(user.name());
+
     }
 }
